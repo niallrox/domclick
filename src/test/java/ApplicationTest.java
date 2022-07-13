750955769c4979511ec92281961dcd5cc18ecaea
niallrox <Folene1234>
2022-07-13 16:27:32 +0300

    Исправлен тест

--- src/test/java/ApplicationTest.java
+++ src/test/java/ApplicationTest.java
@@ -2,15 +2,14 @@ import com.sberbank.domclick.model.Data;
 import com.sberbank.domclick.system.Application;
 import org.junit.jupiter.api.Test;
 
-import java.util.concurrent.BrokenBarrierException;
-import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
 
+import static org.awaitility.Awaitility.await;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 class ApplicationTest {
-    private final CyclicBarrier cyclicBarrier = new CyclicBarrier(8 + 1);
     private final Application application = new Application();
 
     @Test
@@ -18,25 +17,23 @@ class ApplicationTest {
         assertDoesNotThrow(this::execute);
     }
 
-    private void execute() throws InterruptedException, BrokenBarrierException {
+    private void execute() {
         var executorService = Executors.newFixedThreadPool(8);
         IntStream.range(0, 8).forEach((i) -> executorService.submit(this::run));
-        cyclicBarrier.await();
-        cyclicBarrier.await();
+        executorService.shutdown();
+        await().atMost(15, TimeUnit.SECONDS).until(executorService::isTerminated);
     }
 
     private void run() {
         try {
-            cyclicBarrier.await();
             application.handleData(buildData());
-            cyclicBarrier.await();
-        } catch (InterruptedException | BrokenBarrierException e) {
+        } catch (InterruptedException e) {
             throw new RuntimeException();
         }
     }
 
 
     private Data buildData() {
-        return new Data(String.valueOf((int) (Math.random() * 2)), "json");
+        return new Data(String.valueOf((int) (Math.random())), "json");
     }
 }
